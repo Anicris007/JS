710218ce4ae76d20eb99715837a7946772c0bb44
Anirudh Katkar <anikatkar007@gmail.com>
2023-12-24 15:27:16 +0530

    Done

---
+++ hello.js
@@ -0,0 +1,10 @@
+var name = "Anirudh";
+var age = 27;
+var hasHobbies = true;
+
+function summariseUser(uName,uAge,uHobbies)
+{
+    return 'User name is = ' +uName+' is '+uAge+' old & has hobbies = '+hasHobbies+'.';
+}
+
+console.log(summariseUser(name,age,hasHobbies));
